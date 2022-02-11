ff0e52cfea64fcdcc123574f33612ada780540c6
FishingHacks <thejominingcraft@gmail.com>
2022-02-11 18:31:11 +0100

    resolved issue https://github.com/FishingHacks/express-reauth/issues/1

--- api.js
+++ api.js
@@ -17,7 +17,7 @@ module.exports = class {
   }
 
   async getUser(secret) {
-    if (!id || !token) throw new Error(
+    if (!this.id || !this.token) throw new Error(
       `[RE-AUTH-API]: ID or Token missing!`
     );
     let returnValue;
